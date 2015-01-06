b6ea8bed2e09d37c1313ec615905a22b1adc3a57
Brian Lee <brianlee@trunkclub.com>
2015-01-06 16:14:59 -0600

    File format and directory
    JPG/PNG format option
    Update Android directory name
    Use 3x as default (xxhdpi)
    prefix/suffix off by default

--- library/config.js
+++ library/config.js
@@ -1,7 +1,7 @@
 configVars = {
     
     //Include a prefix? [yes/no]
-    includePrefix: "yes", 
+    includePrefix: "no", 
     
 	// By default, it will prompt you to enter a prefix (examples: launcher, menu, stat_notify, tab, dialog). 
 	// Un-comment the next line to set a prefix manually instead: 
@@ -9,7 +9,7 @@ configVars = {
 	
 	
     // Do you want to include a suffix? [yes/no]
-    includeSuffix: "yes",
+    includeSuffix: "no",
     
     // The default suffix is set to the page name (examples: normal, pressed, focused, disabled, selected). 
     // Un-comment the next line to set a custom one manually instead:
@@ -17,7 +17,7 @@ configVars = {
 	
 	
 	// This plugin works best if you design @1x, but if you design @2x (or 3x, or 4x) you can change this number here:
-	baseSize: 1,
+	baseSize: 3, //xxhdpi
 	
 	// Include the asset width at the end of the file name? [yes/no]
 	size: "no"
